7d943aa45cf2094f34a67c3a9b1d9fdb11e8608d
nopparootSuree <nopparoot.su@gmail.com>
2023-06-21 13:42:35 +0700

    update product controller

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,36 +1,65 @@
-import { RequestHandler } from "express";
+import { RequestHandler, Request, Response, NextFunction } from "express";
 import { Products } from "../models/products";
 
-export const createProduct: RequestHandler =async (req, res, next) => {
-    var products = await Products.create({...req.body});
-    return res.status(200).json({ message: "Products create successfully", data: products})
+function handleModleError(error: any, res: Response): void {
+    const errorWithErrors = error as { errors?: any[] };
+    if (errorWithErrors.errors && Array.isArray(errorWithErrors.errors)) {
+        res.status(400).json({ error: 'Validation failed', details: errorWithErrors.errors[0].message });
+    } else {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
-export const deleteProduct: RequestHandler = async (req, res, next) => {
-    const {id} = req.params;
-    const deleteProduct: Products | null = await Products.findByPk(id);
-
-    await Products.destroy({where:{id}})
-
-    return res.status(200).json({ message: "Products deleted successfully", data:deleteProduct})
+export const createProduct: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        var products = await Products.create({...req.body});
+        const affectedRows = products ? 1 : 0
+        if (affectedRows === 1) {
+            return res.status(200).json({ message: "Products create successfully", data: products})
+        }
+    } catch (error) {
+        return handleModleError(error, res)
+    }
 }
 
-export const getAllProduct: RequestHandler =async (req, res, next ) => {
-    const allProducts: Products[] = await Products.findAll()
-
-    return res.status(200).json({message: "Products fetched successfully", data: allProducts})
+export const deleteProduct: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {id} = req.params;
+        const deleteProduct: Products | null = await Products.findByPk(id);
+        await Products.destroy({where:{id}})
+        return res.status(200).json({ message: "Products deleted successfully", data:deleteProduct})
+    } catch (error) {
+        return handleModleError(error, res)
+    }
 }
 
-export const getProductById: RequestHandler = async (req, res, next) => {
-    const {id} = req.params;
-    const product: Products | null = await Products.findByPk(id);
-
-    return res.status(200).json({message: "Products fetched successfully", data: product})
+export const getAllProduct: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const allProducts: Products[] = await Products.findAll()
+        return res.status(200).json({message: "Products fetched successfully", data: allProducts})
+    } catch (error) {
+        return handleModleError(error, res)
+    }
 }
 
-export const updateProduct: RequestHandler =async (req, res, next) => {
-    const {id} = req.params;
-    await Products.update({...req.body},{where:{id}})
-    const updateProduct: Products | null = await Products.findByPk(id);
-    return res.status(200).json({message: "Products fetched successfully", data: updateProduct})
+export const getProductById: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {id} = req.params;
+        const product: Products | null = await Products.findByPk(id);
+        return res.status(200).json({message: "Products fetched successfully", data: product})
+    } catch (error) {
+        return handleModleError(error, res)
+    }
+}
+
+export const updateProduct: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {id} = req.params;
+        await Products.update({...req.body},{where:{id}})
+        const updateProduct: Products | null = await Products.findByPk(id);
+        return res.status(200).json({message: "Products fetched successfully", data: updateProduct})
+    } catch (error) {
+        return handleModleError(error, res)
+    }
+    
 }
