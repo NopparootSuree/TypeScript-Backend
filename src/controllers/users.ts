45dc47d61ea816b8332a9bbb7b2afa9da694debd
nopparootSuree <nopparoot.su@gmail.com>
2023-06-21 14:50:31 +0700

    update user controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,36 +1,87 @@
-import { RequestHandler } from "express";
+import { RequestHandler, Request, Response, NextFunction } from "express";
 import { Users } from "../models/users";
 
-export const createUser: RequestHandler =async (req, res, next) => {
-    var users = await Users.create({...req.body});
-    return res.status(200).json({ message: "user create successfully", data: users})
+function handleModleError(error: any, res: Response): void {
+    const errorWithErrors = error as { errors?: any[] };
+    if (errorWithErrors.errors && Array.isArray(errorWithErrors.errors)) {
+        res.status(400).json({ error: 'Validation failed', details: errorWithErrors.errors[0].message });
+    } else {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
 
-export const deleteUser: RequestHandler = async (req, res, next) => {
-    const {id} = req.params;
-    const deleteUser: Users | null = await Users.findByPk(id);
+export const createUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        var users = await Users.create({...req.body});
+        return res.status(200).json({ message: "Users create successfully", data: users})
 
-    await Users.destroy({where:{id}})
-
-    return res.status(200).json({ message: "User deleted successfully", data:deleteUser})
+    } catch (error) {
+        return handleModleError(error, res)
+    }
 }
 
-export const getAllUser: RequestHandler =async (req, res, next ) => {
-    const allUsers: Users[] = await Users.findAll()
+export const deleteUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {id} = req.params;
+        const deleteUser: Users | null = await Users.findByPk(id);
 
-    return res.status(200).json({message: "Users fetched successfully", data: allUsers})
+        if (deleteUser === null){
+            return res.status(404).json({message: "Users not found"})
+        }
+
+        await Users.destroy({where:{id}})
+        return res.status(200).json({ message: "Users deleted successfully", data:deleteUser})
+
+    } catch (error) {
+        return handleModleError(error, res)
+    }
 }
 
-export const getUserById: RequestHandler = async (req, res, next) => {
-    const {id} = req.params;
-    const user: Users | null = await Users.findByPk(id);
+export const getAllUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const allUsers: Users[] = await Users.findAll()
 
-    return res.status(200).json({message: "Users fetched successfully", data: user})
+        if (allUsers.length === 0){
+            return res.status(200).json({message: "Users fetched successfully", data: "No product data"})
+        }
+
+        return res.status(200).json({message: "Users fetched successfully", data: allUsers})
+
+    } catch (error) {
+        return handleModleError(error, res)
+    }
 }
 
-export const updateUser: RequestHandler =async (req, res, next) => {
-    const {id} = req.params;
-    await Users.update({...req.body},{where:{id}})
-    const updateUser: Users | null = await Users.findByPk(id);
-    return res.status(200).json({message: "Users fetched successfully", data: updateUser})
+export const getUserById: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {id} = req.params;
+        const product: Users | null = await Users.findByPk(id);
+
+        if (product === null){
+            return res.status(404).json({message: "Users not found"})
+        }
+
+        return res.status(200).json({message: "Users fetched successfully", data: product})
+
+    } catch (error) {
+        return handleModleError(error, res)
+    }
+}
+
+export const updateUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {id} = req.params;
+        await Users.update({...req.body},{where:{id}})
+        const updateUser: Users | null = await Users.findByPk(id);
+
+        if (updateUser === null){
+            return res.status(404).json({message: "Users not found"})
+        }
+
+        return res.status(200).json({message: "Users fetched successfully", data: updateUser})
+
+    } catch (error) {
+        return handleModleError(error, res)
+    }
+    
 }
